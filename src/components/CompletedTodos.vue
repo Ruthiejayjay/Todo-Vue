b375729d329afb9fe7fa003cd1653d167aba2b8b
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-08-05 13:03:56 +0100

    added completedTodo Components

---
+++ src/components/CompletedTodos.vue
@@ -0,0 +1,294 @@
+<template>
+  <div class="bg-white">
+    <div class="container px-6 py-5 mx-auto mt-4">
+      <div class="md:flex md:items-center md:justify-between">
+        <div class="flex flex-col items-center mt-4 md:mt-0">
+          <div class="flex flex-row gap-5 w-full lg:max-w-md">
+            <h4 class="text-lg" style="color: #1d262c">Completed</h4>
+            <div class="mt-px">
+              <div
+                class="border rounded-xl px-4"
+                style="background-color: rgba(235, 87, 87, 0.2)"
+              >
+                <h4 class="text-xs" style="color: rgba(235, 87, 87, 1)">
+                  Inactive
+                </h4>
+              </div>
+            </div>
+          </div>
+          <!-- Todo Details 1 -->
+          <div class="flex flex-row gap-7 mt-5 border-b border-b-gray-100">
+            <div class="flex gap-1 mb-px">
+              <svg
+                width="9"
+                height="10"
+                viewBox="0 0 9 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <ellipse
+                  cx="4.5"
+                  cy="5"
+                  rx="4.5"
+                  ry="5"
+                  fill="url(#paint0_linear_1_702)"
+                  fill-opacity="0.2"
+                />
+                <ellipse
+                  cx="4.50078"
+                  cy="5.0493"
+                  rx="2.7"
+                  ry="2.85789"
+                  fill="url(#paint1_linear_1_702)"
+                  fill-opacity="0.7"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_1_702"
+                    x1="0.332031"
+                    y1="-0.334873"
+                    x2="9.8991"
+                    y2="7.95048"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#F3477A" />
+                    <stop offset="1" stop-color="#884CB2" />
+                  </linearGradient>
+                  <linearGradient
+                    id="paint1_linear_1_702"
+                    x1="2"
+                    y1="2"
+                    x2="7.5"
+                    y2="7"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#F3477A" />
+                    <stop offset="1" stop-color="#884CB2" />
+                  </linearGradient>
+                </defs>
+              </svg>
+
+              <h3 class="text-sm" style="color: rgba(189, 189, 189, 1)">
+                Identify the implementation team.
+              </h3>
+            </div>
+            <div class="mb-px">
+              <div
+                class="border rounded-xl px-4"
+                style="background-color: rgba(39, 174, 96, 0.2)"
+              >
+                <h4 class="text-xs" style="color: rgba(39, 174, 96, 1)">
+                  completed
+                </h4>
+              </div>
+            </div>
+            <div class="flex gap-1 mb-px">
+              <svg
+                width="8"
+                height="8"
+                viewBox="0 0 8 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="4" cy="4" r="3" stroke="#EB5757" stroke-width="2" />
+              </svg>
+
+              <h4 class="text-xs text-neutral-200">Minor</h4>
+            </div>
+            <div class="mb-px">
+              <img src="@/assets/disabledFaces.svg" />
+            </div>
+            <div class="mb-px">
+              <div class="bg-gray-100 border rounded-full py-2 px-2">
+                <img src="@/assets/dots.svg" />
+              </div>
+            </div>
+          </div>
+          <!-- Todo Details 2 -->
+         <div class="flex flex-row gap-7 mt-5 border-b border-b-gray-100">
+            <div class="flex gap-1 mb-px">
+              <svg
+                width="9"
+                height="10"
+                viewBox="0 0 9 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <ellipse
+                  cx="4.5"
+                  cy="5"
+                  rx="4.5"
+                  ry="5"
+                  fill="url(#paint0_linear_1_702)"
+                  fill-opacity="0.2"
+                />
+                <ellipse
+                  cx="4.50078"
+                  cy="5.0493"
+                  rx="2.7"
+                  ry="2.85789"
+                  fill="url(#paint1_linear_1_702)"
+                  fill-opacity="0.7"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_1_702"
+                    x1="0.332031"
+                    y1="-0.334873"
+                    x2="9.8991"
+                    y2="7.95048"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#F3477A" />
+                    <stop offset="1" stop-color="#884CB2" />
+                  </linearGradient>
+                  <linearGradient
+                    id="paint1_linear_1_702"
+                    x1="2"
+                    y1="2"
+                    x2="7.5"
+                    y2="7"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#F3477A" />
+                    <stop offset="1" stop-color="#884CB2" />
+                  </linearGradient>
+                </defs>
+              </svg>
+
+              <h3 class="text-sm" style="color: rgba(189, 189, 189, 1)">
+                Identify the implementation team.
+              </h3>
+            </div>
+            <div class="mb-px">
+              <div
+                class="border rounded-xl px-4"
+                style="background-color: rgba(39, 174, 96, 0.2)"
+              >
+                <h4 class="text-xs" style="color: rgba(39, 174, 96, 1)">
+                  completed
+                </h4>
+              </div>
+            </div>
+            <div class="flex gap-1 mb-px">
+              <svg
+                width="8"
+                height="8"
+                viewBox="0 0 8 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="4" cy="4" r="3" stroke="#EB5757" stroke-width="2" />
+              </svg>
+
+              <h4 class="text-xs text-neutral-200">Minor</h4>
+            </div>
+            <div class="mb-px">
+              <img src="@/assets/disabledFaces.svg" />
+            </div>
+            <div class="mb-px">
+              <div class="bg-gray-100 border rounded-full py-2 px-2">
+                <img src="@/assets/dots.svg" />
+              </div>
+            </div>
+          </div>
+          <!-- Todo Details 3 -->
+          <div class="flex flex-row gap-7 mt-5 border-b border-b-gray-100">
+            <div class="flex gap-1 mb-px">
+              <svg
+                width="9"
+                height="10"
+                viewBox="0 0 9 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <ellipse
+                  cx="4.5"
+                  cy="5"
+                  rx="4.5"
+                  ry="5"
+                  fill="url(#paint0_linear_1_702)"
+                  fill-opacity="0.2"
+                />
+                <ellipse
+                  cx="4.50078"
+                  cy="5.0493"
+                  rx="2.7"
+                  ry="2.85789"
+                  fill="url(#paint1_linear_1_702)"
+                  fill-opacity="0.7"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_1_702"
+                    x1="0.332031"
+                    y1="-0.334873"
+                    x2="9.8991"
+                    y2="7.95048"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#F3477A" />
+                    <stop offset="1" stop-color="#884CB2" />
+                  </linearGradient>
+                  <linearGradient
+                    id="paint1_linear_1_702"
+                    x1="2"
+                    y1="2"
+                    x2="7.5"
+                    y2="7"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#F3477A" />
+                    <stop offset="1" stop-color="#884CB2" />
+                  </linearGradient>
+                </defs>
+              </svg>
+
+              <h3 class="text-sm" style="color: rgba(189, 189, 189, 1)">
+                Identify the implementation team.
+              </h3>
+            </div>
+            <div class="mb-px">
+              <div
+                class="border rounded-xl px-4"
+                style="background-color: rgba(39, 174, 96, 0.2)"
+              >
+                <h4 class="text-xs" style="color: rgba(39, 174, 96, 1)">
+                  completed
+                </h4>
+              </div>
+            </div>
+            <div class="flex gap-1 mb-px">
+              <svg
+                width="8"
+                height="8"
+                viewBox="0 0 8 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="4" cy="4" r="3" stroke="#EB5757" stroke-width="2" />
+              </svg>
+
+              <h4 class="text-xs text-neutral-200">Minor</h4>
+            </div>
+            <div class="mb-px">
+              <img src="@/assets/disabledFaces.svg" />
+            </div>
+            <div class="mb-px">
+              <div class="bg-gray-100 border rounded-full py-2 px-2">
+                <img src="@/assets/dots.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="container px-7 py-4 mx-auto">
-   <HomeHeader />
-   <Todos />
+    <HomeHeader />
+    <Todos />
+    <CompletedTodos />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HomeHeader from '@/components/HomeHeader.vue'
-import Todos from '@/components/Todos.vue'
+import HomeHeader from "@/components/HomeHeader.vue";
+import Todos from "@/components/Todos.vue";
+import CompletedTodos from "@/components/CompletedTodos.vue";
 export default {
   name: "HomeView",
   components: {
     HomeHeader,
-    Todos
+    Todos,
+    CompletedTodos,
   },
 };
 </script>
